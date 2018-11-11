0ff96541252d05dd3b0fe38a3d15466b50fd8662
Marco Nunnari <marnunna@gmail.com>
2018-11-11 13:21:18 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -10,14 +10,7 @@ defmodule Rename.Mixfile do
       start_permanent: Mix.env == :prod,
       description: description(),
       package: package(),
-      deps: deps(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        "coveralls": :test, 
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
-      ]
+      deps: deps()
     ]
   end
 
